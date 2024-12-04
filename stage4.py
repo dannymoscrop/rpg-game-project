5a7d71baecc4ec77cf700993eabb626be0fce3a0
luciaci77 <lucia.cizmarova77@gmail.com>
2024-12-04 13:14:12 +0000

    Add files via upload

---
+++ stage4.py
@@ -0,0 +1,23 @@
+player_hp = 11
+has_key = False
+has_password = "key"
+
+def stage_4_cave(player_hp, has_password, has_key):
+    print("\nStage 4: The Cave")
+    print("You arrived at the entrance of dark cave. Inside you find Agnes, the powerful witch. She asks you for password")
+
+    if has_password and has_key:
+        print(f"The password is {has_password}. I have also found a key in the swamp, maybe you can tell me what it opens")
+        print("She will point at chest in the dark corner of the cave and explains that the key opens the chest.You will get powerful weapon, which  will grant you +2 damage bonus in next stage")
+    elif has_password and not has_key:
+        print(f"The password is {has_password}.")
+        print("As a token  I will grant you healing spell")
+        player_hp += 1
+        print(f"Your HP is now {player_hp}")
+    else:
+        print("I have no password")
+        print("She distrust you and refused to help you further")
+
+    print("Proceed to stage 5: The Ruins")
+    
+stage_4_cave(player_hp, has_password, has_key)

3473c6b0d207f1a9b8151c9e941f4cb96a21f32d
luciaci77 <lucia.cizmarova77@gmail.com>
2024-12-04 14:52:40 +0000

    Add files via upload

--- rpgame.py
+++ rpgame.py
@@ -1,33 +1,39 @@
 from random import randint
 
 player_hp = 14
-monster_hp = 10
-game_running = player_hp > 0 and monster_hp > 0
 
-def final_battle(player_hp, monster_hp):
-  while game_running:
+
+def final_battle(player_hp):
+   monster_hp = 10
+   game_running = player_hp > 0 and monster_hp > 0
+
+   while game_running:
+      print("Welcome to the final battle: The Ruins")
+      print(f"Your HP is now {player_hp}")
     
-    input("Press Enter to continue")
+      input("Press Enter to continue")
             
-    player_attack = randint(1, 4)
-    monster_hp -= player_attack
-    print(f"You attack hooded figure for {player_attack}")
-    print(f"Hooded figure damage is now {monster_hp}")
+      player_attack = randint(1, 4)
+      monster_hp -= player_attack
+      print(f"You attack hooded figure for {player_attack}")
+      print(f"Hooded figure damage is now {monster_hp}")
     
-    if monster_hp <= 0:
-      break
+      if monster_hp <= 0:
+        break
 
-    monster_attack = randint(1, 4)
-    player_hp -= monster_attack
-    print(f"Hooded figure attacked you for {monster_attack} ")
-    print(f"Your HP is now {player_hp}")
+      monster_attack = randint(1, 4)
+      player_hp -= monster_attack
+      print(f"Hooded figure attacked you for {monster_attack} ")
+      print(f"Your HP is now {player_hp}")
     
-    if player_hp <= 0:
-      break
+      if player_hp <= 0:
+        break
 
-if player_hp > 4:
-  print("It's a win")
-elif player_hp <= 0:
-  print("Defeat")
-else:
-  print("It's a win, but...")
+   if player_hp > 4:
+      print("It's a win")
+   elif player_hp <= 0:
+      print("Defeat")
+   else:
+    print("It's a win, but...")
+
+final_battle(player_hp)

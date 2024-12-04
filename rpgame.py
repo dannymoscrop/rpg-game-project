a6685202853f9236c5bec29062ed53d28dc445f7
luciaci77 <lucia.cizmarova77@gmail.com>
2024-12-04 14:13:40 +0000

    rpgame.py-stage5
    Updated code for stage 5

--- rpgame.py
+++ rpgame.py
@@ -1,15 +1,33 @@
 from random import randint
 
-player_health = 14
-hooded_figure_health = 10
-game_running = True
+player_hp = 14
+monster_hp = 10
+game_running = player_hp > 0 and monster_hp > 0
 
-def begin_game():
+def final_battle(player_hp, monster_hp):
   while game_running:
-    print("Hero attack")
-        
-    if hero_choice == 1:
-      player_attack_damage = randint(1, 4)
-      print(f"{hero_name} attack for {player_attack_damage}")
-      
+    
+    input("Press Enter to continue")
+            
+    player_attack = randint(1, 4)
+    monster_hp -= player_attack
+    print(f"You attack hooded figure for {player_attack}")
+    print(f"Hooded figure damage is now {monster_hp}")
+    
+    if monster_hp <= 0:
+      break
 
+    monster_attack = randint(1, 4)
+    player_hp -= monster_attack
+    print(f"Hooded figure attacked you for {monster_attack} ")
+    print(f"Your HP is now {player_hp}")
+    
+    if player_hp <= 0:
+      break
+
+if player_hp > 4:
+  print("It's a win")
+elif player_hp <= 0:
+  print("Defeat")
+else:
+  print("It's a win, but...")

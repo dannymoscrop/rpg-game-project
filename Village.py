b94b43043089e6f9bfbda3cb7c69e810d24f4b3b
James-CFJ-Sharples <james.francis.sharples@gmail.com>
2024-12-04 14:23:52 +0000

    Update Village.py
    latest. fixed bugs, runs, needs def and hp adding

--- Village.py
+++ Village.py
@@ -1,8 +1,7 @@
-Village
-print ("You head out of the castle and back towards the village. It is eerily quiet apart the the tapping of a blind woman’s stick. Her glossy eyes seem to peer into your soul. Speaking as if she knows everything you have ever done. 'Yes… Yes… I see what you will do. Your path diverges here hero. Answer my riddle and I will show you the way forwards.')
-    print ("I open locks yet hold no teeth, Silent I stand, with secrets beneath. Though I am small, I hold great might, To open doors and grant you sight. What am I?")
-input
-    if answer is "key"
-      print (“'Very good. Go down that path through the swamp, there is a key, if you find it you will be able to claim our Kingdoms sacred sword.'") 
-          and ”hp" = "hp"+1 and send to "swamp"
-    else send to "cave"" and "hp"=="hp"-2!
+print("You head out of the castle and back towards the village. It is eerily quiet apart the the tapping of a blind womans stick. Her glossy eyes seem to peer into your soul. Speaking as if she knows everything you have ever done. Yes… Yes… I see what you will do. Your path diverges here hero. Answer my riddle and I will show you the way forwards.")
+print("I open locks yet hold no teeth, Silent I stand, with secrets beneath. Though I am small, I hold great might, To open doors and grant you sight. What am I?")
+while True:
+    answer = input(" > ").lower()
+    if answer == "key":
+      print("Very good. Go down that path through the swamp, there is a key, if you find it you will be able to claim our Kingdoms sacred sword.") 
+    else: print("I am afraid not, the cave is down that path. But are you prepared?")
